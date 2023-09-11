85a05dd66c933aef494ef87ba5942d0ef5bc6216
Omar <134083283+omardiosdado@users.noreply.github.com>
2023-09-11 11:53:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,8 @@ hide_default_format = """
        """
 st.markdown(hide_default_format, unsafe_allow_html=True)
 
-st.image("https://i.imgur.com/XQ0ePg2.png", use_column_width='auto')
+# st.image("https://i.imgur.com/XQ0ePg2.png", use_column_width='auto')
+st.title('Procesador de Leads')
 st.caption(':turtle: V1.01')
 
 col1, col2 =st.columns([1,3])
