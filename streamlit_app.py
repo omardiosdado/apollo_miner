f48e84cd05695523289e56e27161c79183f46cf5
Omar <134083283+omardiosdado@users.noreply.github.com>
2023-09-11 10:40:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ client = gspread.authorize(credentials)
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-# @st.cache_data(ttl=600)
+@st.cache_data(ttl=600)
 def load_data(url, sheet_name):
     sh = client.open_by_url(url)
     df = pd.DataFrame(sh.worksheet(sheet_name).get_all_records())
